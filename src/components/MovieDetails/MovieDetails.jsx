aaf9eeddbc8de09d2b9f86551defa55b6c1e8223
Ivan <120274876+Gelsomin0@users.noreply.github.com>
2023-08-06 10:45:23 +0300

    The problem is resolved

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import getFetchData from "tools/fetchData";
+import imageBaseURL from "tools/imageBaswURL";
+import css from './MovieDetails.module.css';
 
 export default function MovieDetails() {
     const [fetchData, setFetchData] = useState({})
     const { movieId } = useParams();
     useEffect(() => {
+        setFetchData([]);
         const handleFetchData = async () => {
             try {
                 await getFetchData('movieID', movieId)
@@ -19,12 +22,27 @@ export default function MovieDetails() {
         }
 
         handleFetchData();
-        console.log(fetchData);
     }, []);
 
+
     return (
-        <div>
+        <div className={css.movie_container}>
+            <div>
+                {
+                    fetchData.poster_path &&
+                    <img
+                        className={css.poster}
+                        src={`${imageBaseURL()}${fetchData.poster_path}`}
+                    />
+                }    
+            </div>
             
+            <h2>{fetchData?.title || fetchData?.name} 
+                {
+                    fetchData.release_date &&
+                    <span>{` (${fetchData.release_date.slice(0, 4)})`}</span>
+                }
+            </h2>
         </div>
-    );
+    ); 
 }
---
+++ src/tools/imageBaswURL.js
@@ -0,0 +1,4 @@
+export default function imageBaseURL() {
+    const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500/';
+    return IMAGE_BASE_URL;
+}
