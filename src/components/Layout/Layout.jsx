da8159462c34450c802676b887601d034bfe4b67
Ivan <120274876+Gelsomin0@users.noreply.github.com>
2023-08-06 18:27:57 +0300

    Added some styles

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,34 @@
-import { Link, NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import css from './Layout.module.css';
+import styled from 'styled-components';
+
+const StyledNavLink = styled(NavLink)`
+    font-size: 20px;
+    color: #000;
+
+    &.active {
+        background-color: orange;
+        padding: 5px 15px;
+        border-radius: 4px;
+        box-shadow: 2px 2px 3px 0px rgba(0,0,0,0.75);
+    }
+`
+
+const Header = styled.header`
+    padding: 10px 30px;
+    border-bottom: 2px solid #000;
+`
 
 export default function Layout() {
     return (
         <div className={css.container}>
-            <header>
-                <nav>
-                    <NavLink to='/'>Home Page</NavLink>
-                    <NavLink to='/movies'>Movies</NavLink>
+            <Header className={css.header}>
+                <div className={css.logo}>iMOVIE</div>
+                <nav className={css.nav_bar}>
+                    <StyledNavLink to='/'>Home Page</StyledNavLink>
+                    <StyledNavLink to='/movies'>Movies</StyledNavLink>
                 </nav>
-            </header>
+            </Header>
             <Outlet/>
         </div>
     );
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,35 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import getFetchData from "tools/fetchData";
+import styled from 'styled-components';
+
+const Li = styled.li`
+    color: #000;
+    background-color: #f8f7f7;
+    margin: 10px 0;
+    padding: 7px 20px;
+    border-radius: 7px;
+
+    &:hover {
+        color: #f8f7f7;
+        background-color: #000;
+    }
+`
+
+const StyledLink = styled(Link)`
+    font-size: 18px;
+    font-weight: 700;
+    color: #606060;
+    margin-left: 30px;
+    margin-bottom: 8px;
+
+    &:hover {
+        color: #fff;
+    }
+`
 
 export default function Home() {
     const [trandMovies, setTrandMovies] = useState([]);
-    // const isFirstRender = useRef(true);
 
     useEffect(() => {
         const handleFetchData = async () => {
@@ -24,10 +49,12 @@ export default function Home() {
         <ul>
             {trandMovies.map((movie) => {
                 return (
-                    <li key={movie.id}>
-                        <Link to={`movies/${movie.id}`}>{movie?.title || movie?.name}</Link>
+                    <Li key={movie.id}>
+                        <StyledLink to={`movies/${movie.id}`}>
+                            {movie?.title || movie?.name}
+                        </StyledLink>
                         <p>{movie.overview}</p>
-                    </li>
+                    </Li>
                 );
             })}
         </ul>
